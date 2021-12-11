35b3b9a287ed4e1a0210c41af03e65cc10afaef7
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-12-11 17:17:05 +0900

    【微修正】QuestionBoxControllerにコメントの追加

--- src/main/java/com/example/demo/controller/QuestionBoxController.java
+++ src/main/java/com/example/demo/controller/QuestionBoxController.java
@@ -7,15 +7,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/question")
 public class QuestionBoxController {
 	
+	////////////////////////////////
+	//　　　　　インデックス表示　　　　//
+	////////////////////////////////
+	
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
+	////////////////////////////////
+	//　　　　　　　ユーザー　　　　　　//
+	////////////////////////////////
+	/**
+	 * ユーザー登録画面を表示させるメソッド
+	 * @return
+	 */
+	@RequestMapping("/user-register")
+	public String userRegister() {
+		return "user-confirm";
+	}
 	
 	
+	/**
+	 * ユーザー登録情報を受け取る
+	 * @return
+	 */
+	public String userSignUp() {
+		return "redirect:user-login";
+	}
 	
-	@RequestMapping("/login")
+	/**
+	 * ユーザーログイン画面を表示する
+	 * @return
+	 */
+	@RequestMapping("/user-login")
 	public String userLogin() {
 		return "user-login";
 	}
