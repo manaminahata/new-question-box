2823a1e62fbe35f33b9688a1820a5cce7a8e8b01
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-12-12 10:03:38 +0900

    QuestionBoxControllerに管理者登録を追加（パス指定のみで中身は後ほど追加する）

--- src/main/java/com/example/demo/controller/QuestionBoxController.java
+++ src/main/java/com/example/demo/controller/QuestionBoxController.java
@@ -30,11 +30,23 @@ public class QuestionBoxController {
 	
 	
 	/**
-	 * ユーザー登録情報を受け取る
+	 * ユーザー登録情報を受け取り、登録内容確認画面に遷移する
 	 * @return
 	 */
+	@RequestMapping("/user-signup")
 	public String userSignUp() {
 		return "redirect:user-login";
+	}
+	
+	/**
+	 * 登録した内容を表示する
+	 * 登録内容を修正する場合は「修正ボタン」を押すと「userSignUpメソッド」に遷移する
+	 * 登録内容に誤りがない場合は「登録ボタン」を押すとログイン画面に遷移する
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String userConfirm() {
+		return "user-confirm";
 	}
 	
 	/**
@@ -45,4 +57,19 @@ public class QuestionBoxController {
 	public String userLogin() {
 		return "user-login";
 	}
+	
+	////////////////////////////////
+	//　　　　　　　 管理者　　　　　  //
+	////////////////////////////////
+	
+	/**
+	 * 管理者登録画面を表示させるメソッド
+	 * @return
+	 */
+	@RequestMapping("/administrator-register")
+	public String administratorRegister() {
+		return "administrator-register";
+	}
+	
+	
 }
