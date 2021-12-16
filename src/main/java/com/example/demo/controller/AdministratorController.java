12bb20f8aa79a1039cced4b595222f9c96602c81
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-12-16 22:46:24 +0900

    管理者用のControllerの作成

---
+++ src/main/java/com/example/demo/controller/AdministratorController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/questionbox")
+public class AdministratorController {
+
+//	/**
+//	 * 管理者登録画面を表示させるメソッド
+//	 * @return
+//	 */
+//	@RequestMapping("/administrator-register")
+//	public String administratorRegister() {
+//		return "administrator-register";
+//	}
+//	
+//	/**
+//	 * ユーザーログイン画面を表示する
+//	 * @return
+//	 */
+//	@RequestMapping("/administrator-login")
+//	public String administratorLogin() {
+//		return "administrator-login";
+//	}
+	
+}
